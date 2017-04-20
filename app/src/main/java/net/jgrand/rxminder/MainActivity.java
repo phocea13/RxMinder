2fb73483a37218984c999a3134e02e0be293bb7c
phocea13 <grandmaire@gmail.com>
2017-04-20 12:55:52 -0400

    Added button press logs

--- app/src/main/java/net/jgrand/rxminder/MainActivity.java
+++ app/src/main/java/net/jgrand/rxminder/MainActivity.java
@@ -9,7 +9,12 @@ import android.widget.Chronometer;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
+import org.threeten.bp.LocalDateTime;
+import org.threeten.bp.format.DateTimeFormatter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -20,6 +25,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private boolean connected;
     private Button deviceButton;
     private int pressCount = 0;
+    private List<LocalDateTime> instantsMedsTaken;
+    private TextView medsTakenLogTextView;
+    private StringBuilder sbMedsTaken;
+
+    final String dbDatePattern = "yyyy-MM-dd @ HH:mm:ss";
+    final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(dbDatePattern);
 
 
     @Override
@@ -30,6 +41,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         connectButton = (Button) findViewById(R.id.connectButton);
         disconnectButton = (Button) findViewById(R.id.disconnectButton);
         deviceButton = (Button) findViewById(R.id.buttonPressButton);
+        medsTakenLogTextView = (TextView) findViewById(R.id.medsTakenLog);
         connectButton.setOnClickListener(this);
         disconnectButton.setOnClickListener(this);
         deviceButton.setOnClickListener(this);
@@ -40,7 +52,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         connectTimer = (Chronometer) findViewById(R.id.connectionTimer);
         connectTimer.setBase(SystemClock.elapsedRealtime());
         disconnectButton.setEnabled(false);
-        buttonPressCount.setEnabled(false);
+        deviceButton.setEnabled(false);
+        instantsMedsTaken = new ArrayList<>();
+        sbMedsTaken = new StringBuilder("Meds Log:\n");
 
     }
 
@@ -49,34 +63,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch(v.getId()){
             case R.id.connectButton:
-                //handle
                 if(!connected) {
                     connectTimer.setBase(SystemClock.elapsedRealtime());
                     connectTimer.start();
                     connectionStatus.setText("Connected");
                     connected = true;
-                    connectButton.setEnabled(false);
-                    disconnectButton.setEnabled(true);
-                    buttonPressCount.setEnabled(true);
+                    connectButton.setEnabled(!connected);
+                    disconnectButton.setEnabled(connected);
+                    deviceButton.setEnabled(connected);
                 } else {
                     Toast.makeText(this, "Already connected", Toast.LENGTH_LONG).show();
                 }
                 break;
             case R.id.disconnectButton:
-                //handle
                 connectionStatus.setText("Disconnected");
                 connectTimer.stop();
                 connected = false;
                 pressCount = 0;
                 buttonPressCount.setText("0");
-                connectButton.setEnabled(true);
-                disconnectButton.setEnabled(false);
-                buttonPressCount.setEnabled(false);
+                connectButton.setEnabled(!connected);
+                disconnectButton.setEnabled(connected);
+                deviceButton.setEnabled(connected);
                 break;
             case R.id.buttonPressButton:
                 if(connected){
                     pressCount++;
                     buttonPressCount.setText(String.format("%d times", pressCount));
+                    LocalDateTime now = LocalDateTime.now();
+                    instantsMedsTaken.add(now);
+                    sbMedsTaken.append(dateTimeFormatter.format(now));
+                    sbMedsTaken.append("\n");
+                    medsTakenLogTextView.setText(sbMedsTaken.toString());
                 } else {
                     Toast.makeText(this, "You must be connected first.", Toast.LENGTH_LONG).show();
                 }
